8e819b3b475b88a75f80d3c3c4819a114c552e4e
Oscar Li <nibbles913@gmail.com>
2015-01-21 17:08:47 -0500

    .DS_Store removal, experiment updates

--- pl/experiment.sh
+++ pl/experiment.sh
@@ -62,6 +62,6 @@ do
 done < nodes.txt
 
 # Inserts into database
-# cd /mnt/external-drive/
-# nohup bash ~/scripts/insertIntoDB2.sh "$ENTRY_DIRNAME" &
-# nohup bash ~/scripts/insertIntoDB2.sh "$EXIT_DIRNAME" &
+cd /mnt/external-drive/
+nohup bash ~/scripts/insertIntoDB2.sh "$ENTRY_DIRNAME" &
+nohup bash ~/scripts/insertIntoDB2.sh "$EXIT_DIRNAME" &
