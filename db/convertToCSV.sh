35be270f7639870268affe8bd20ed38077a30d0f
Oscar Li <nibbles913@gmail.com>
2015-02-02 16:34:40 -0500

    testing

--- db/convertToCSV.sh
+++ db/convertToCSV.sh
@@ -11,32 +11,42 @@ CURR_DIR=`pwd`
 function convert
 {
 	srcIP=`sed -n 2p < "$1" | cut -d "(" -f2 | cut -d ")" -f1` 
-	if [ "$srcAS" = "*" ];
-	then
-		srcAS="AS"`whois -h whois.cymru.com " -v $srcIP" | tail -1 | cut -f1 -d" "`
-	else 
-		srcAS=`sed -n 2p < "$1" | cut -d "[" -f2 | cut -d "]" -f1`
-	fi
+	# if [ "$srcAS" = "*" ];
+	# then
+	# 	srcAS="AS"`whois -h whois.cymru.com " -v $srcIP" | tail -1 | cut -f1 -d" "`
+	# else 
+	# 	srcAS=`sed -n 2p < "$1" | cut -d "[" -f2 | cut -d "]" -f1`
+	# fi
 
 	destIP=`echo $1 | awk -F"/" '{print $NF}' | cut -f1 -d "("`
-	destAS="AS"`whois -h whois.cymru.com " -v $destIP" | tail -1 | cut -f1 -d" "`
-	tstamp=`echo $1 | cut -d "(" -f2 | cut -d ")" -f1`
-	path=`cat "$1"`
+	# destAS="AS"`whois -h whois.cymru.com " -v $destIP" | tail -1 | cut -f1 -d" "`
+	# tstamp=`echo $1 | cut -d "(" -f2 | cut -d ")" -f1`
+	# path=`cat "$1"`
 
-	echo $path | grep -o '\[[AS[0-9\/]*]*\]' | awk '!x[$0]++' > ~/tempCSV.txt
-	aspath=`cat ~/tempCSV.txt` 
-	numases=`wc -l < ~/tempCSV.txt | tr -d " \t\n\r"` 
+	# echo $path | grep -o '\[[AS[0-9\/]*]*\]' | awk '!x[$0]++' > ~/tempCSV.txt
+	# aspath=`cat ~/tempCSV.txt` 
+	# numases=`wc -l < ~/tempCSV.txt | tr -d " \t\n\r"` 
 
-	# A traceroute is invalid if it has more than 2 routers that timed out
-	valid="true"
-	if [ `grep -o "\* \* \*" "$1" | wc -l` -ge 2 ]; 
-	then 
-		valid="false"
-	fi 
+	# # A traceroute is invalid if it has more than 2 routers that timed out
+	# valid="true"
+	# if [ `grep -o "\* \* \*" "$1" | wc -l` -ge 2 ]; 
+	# then 
+	# 	valid="false"
+	# fi 
 
 	# Creates CSV entry
-	entry="$tstamp~$srcIP~$srcAS~$destIP~$destAS~$path~$aspath~$numases~$type~$valid"
+	#entry="$tstamp~$srcIP~$srcAS~$destIP~$destAS~$path~$aspath~$numases~$type~$valid"
 
+	if [ `${#destip}` -ge 16 ];
+	then
+		echo "$destip"
+	fi
+
+	if [ `${#srcip}` -ge 16 ];
+	then
+		echo "$srcip"
+	fi
+	
 	# For debug
 	# echo "$1"
 	# echo "HOST: $host"
