f866a386413f520636e0b88c6668222f80be2df7
Oscar Li <nibbles913@gmail.com>
2015-02-04 00:36:52 -0500

    tail is better than sed

--- translateHost.sh
+++ translateHost.sh
@@ -8,7 +8,7 @@
 while read line
 do
 	IP="`dig +short $line`" 
-	AS="`whois -h whois.cymru.com " -v $IP" | sed -n 2p | cut -f1 -d" "`"
+	AS="`whois -h whois.cymru.com " -v $IP" | tail -1 | cut -f1 -d" "`"
 
 	echo -e "$line \t $IP \t $AS"
 done < $1
