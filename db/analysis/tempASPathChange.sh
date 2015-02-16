bd03d2592c5c9e901d67c0de5d13558250adda18
Oscar Li <nibbles913@gmail.com>
2015-02-16 18:15:01 -0500

    file location change

--- db/analysis/tempASPathChange.sh
+++ db/analysis/tempASPathChange.sh
@@ -6,12 +6,12 @@
 # Author: Oscar Li
 #--------------------------------------------------------------
 
-mkdir aspath
+mkdir aspathcounts
 while read pair
 do
 	srcas=`echo $pair | cut -d'~' -f1` 
 	destas=`echo $pair | cut -d'~' -f2`
-	query="\copy (SELECT COUNT(aspath) FROM paths WHERE srcas='$srcas' AND destAS='$destas' AND valid = true) TO '~/aspath/$srcas-$destas'"
+	query="\copy (SELECT COUNT(aspath) FROM paths WHERE srcas='$srcas' AND destAS='$destas' AND valid = true) TO '~/aspathcounts/$srcas-$destas'"
 	psql -U oli -d raptor -w -c "$query" &
 	sleep 5
 done < asPairs.txt
