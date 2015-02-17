1c7bcfbcce473ebafaa4dc08325333fb2af5f9b0
Oscar Li <nibbles913@gmail.com>
2015-02-16 21:33:35 -0500

    file piping error fix

--- db/analysis/asPathChange.sh
+++ db/analysis/asPathChange.sh
@@ -11,15 +11,14 @@ while read pair
 do
 	srcas=`echo $pair | cut -d'~' -f1` 
 	destas=`echo $pair | cut -d'~' -f2`
-	path="~/asPathChange/$srcas-$destas"
+	file="$srcas-$destas"
 
-	query="\copy (SELECT aspath FROM paths WHERE srcas='$srcas' AND destAS='$destas' AND valid = true) TO '$path'"	
+	query="\copy (SELECT aspath FROM paths WHERE srcas='$srcas' AND destAS='$destas' AND valid = true) TO '~/asPathChange/$file'"	
 	psql -U oli -d raptor -w -c "$query"
 
-	awk '{amount [$1]+=1} END 
-	 	 {for (name in amount) print name, amount[name] | "sort -k2 -nr"}' $path | sed 's/\\n/~/g' | cut -d' ' -f1 > $path.data
-	rm $path 
-	numASes=`python tor-traceroutes/db/analysis/asPathChange_CSV.py $path.data`
+	awk '{amount [$1]+=1} END {for (name in amount) print name, amount[name] | "sort -k2 -nr"}' ~/asPathChange/$file | sed 's/\\n/~/g' | cut -d' ' -f1 > ~/asPathChange/"$file.data"
+	rm ~/asPathChange/"$file" 
+	numASes=`python tor-traceroutes/db/analysis/asPathChange_CSV.py ~/asPathChange/$file.data`
 
 	echo "$srcas $destas $numASes"
 done < asPairs.txt
