569327c3404ca38cfacbe55591fa500e1611e409
Oscar Li <nibbles913@gmail.com>
2015-03-11 13:05:09 -0400

    incorrect file path bug fix

--- pl/trace.sh
+++ pl/trace.sh
@@ -1,34 +1,72 @@
 #!/bin/bash
 
-rm -rf entryResults exitResults entryExitResults
+rm -rf entryResults exitResults entryExitResults temp
 
-mkdir entryResults exitResults entryExitResults
+mkdir entryResults exitResults entryExitResults temp
 
 # Prevent accidental DDOS
-shuf entryRelays.txt > rand_entry.txt
-shuf exitRelays.txt > rand_exit.txt  
-shuf entryExitRelays.txt > rand_entryExit.txt
+shuf as_ip_entryRelays.txt > rand_entry.txt
+shuf as_ip_exitRelays.txt > rand_exit.txt  
+shuf as_ip_entryExitRelays.txt > rand_entryExit.txt
+
+srcAS=`awk '{print $1}' info.txt`
+srcIP=`awk '{print $2}' info.txt`
+
+function convert # (filepath, destIP, destAS, type) # 
+{
+	filePath=$1
+	path=`cat $filePath`
+	file=`basename $1`
+	destIP=$2
+	destAS=$3
+	type=$4
+	tstamp=`date +%m-%d-%y\ %k:%M`
+
+	# Extracts AS level path
+	echo "begin" > temp/"$file"
+	tail -n +2 "$filePath" | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+" >> temp/"$file"
+	echo "end" >> temp/"$file"
+	aspath=`nc whois.cymru.com 43 < temp/"$file" | awk '{print $1}' | awk '!x[$0]++' | tail -n +2 | grep -o "[0-9]\+" | sed 's/$/]/' | sed 's/^/[AS/'`
+	rm temp/"$file"
+
+	numases=`echo "$aspath" | wc -l | tr -d " \t\n\r"` 
+
+	# A traceroute is invalid if it has more than 2 routers that timed out
+	valid="true"
+	if [ `grep -o "\* \* \*" "$filePath" | wc -l` -ge 2 ]; 
+	then 
+		valid="false"
+	fi 
+
+	echo "$tstamp~$srcIP~$srcAS~$destIP~$destAS~$path~$aspath~$numases~$type~$valid" | sed ':a;N;$!ba;s/\n/\\n/g'
+}
 
 # Performs traceroute to each Tor entry guard
-while read line           
+while read line 
 do 
-	tstamp=`date +%m-%d-%y\ %k:%M`
-	FILENAME=$line"($tstamp)"    
-	traceroute $line > entryResults/"$FILENAME"
+	entryAS=`echo $line | awk '{print $1}'`
+	entryIP=`echo $line | awk '{print $2}'`
+
+	traceroute $entryIP > entryResults/"entry_$entryIP"
+	convert entryResults/"entry_$entryIP" $entryIP $entryAS Entry >> entry.csv
 done < rand_entry.txt &
 
 # Performs traceroute to each Tor exit guard
-while read line           
+while read line     
 do 
-	tstamp=`date +%m-%d-%y-%k:%M`
-	FILENAME=$line"($tstamp)"    
-	traceroute $line > exitResults/"$FILENAME"
+	exitAS=`echo $line | awk '{print $1}'`
+	exitIP=`echo $line | awk '{print $2}'`
+
+	traceroute $exitIP > exitResults/"exit_$exitIP"
+	convert exitResults/"exit_$exitIP" $exitIP $exitAS Exit >> exit.csv 
 done < rand_exit.txt &
 
 # Performs traceroute to each Tor entry/exit guard
-while read line           
+while read line       
 do 
-	tstamp=`date +%m-%d-%y-%k:%M`
-	FILENAME=$line"($tstamp)"    
-	traceroute $line > entryExitResults/"$FILENAME"
+	entryExitAS=`echo $line | awk '{print $1}'`
+	entryExitIP=`echo $line | awk '{print $2}'`
+
+	traceroute $entryExitIP > entryExitResults/"entryExit_$entryExitIP"
+	convert entryExitResults/"entryExit_$entryExitIP" $entryExitIP $entryExitAS Both >> entryExit.csv
 done < rand_entryExit.txt &
